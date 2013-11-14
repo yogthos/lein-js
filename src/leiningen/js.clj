67a691b534ab657ef1fa30f1f97a32ab6901c244
Yogthos <dmitri.sotnikov@uhn.on.ca>
2013-11-14 15:29:48 -0500

    updated closure lib dependency

--- src/leiningen/js.clj
+++ src/leiningen/js.clj
@@ -1,7 +1,7 @@
 (ns leiningen.js
-  (:import [java.io File])
-  (:use [clojure.java.io :only [file make-parents]])
-  (:require [lein-js.closure :as closure]))
+  (:import java.io.File)
+  (:require [clojure.java.io :refer [file make-parents]]
+            [lein-js.closure :as closure]))
 
 (defn- add-path
   [path file]
@@ -10,32 +10,54 @@
 (defn- src-path
   [project]
   (add-path (:root project)
-	    (or (:src (:js project)) "src/js")))
+            (or (:src (:js project)) "src/js")))
 
 (defn- deploy-path
   [project]
   (add-path (:root project)
-	    (or (:deploy (:js project)) "war/js")))
+            (or (:deploy (:js project)) "war/js")))
 
 (defn- compile-bundle
   [inputs output project devel]
   (let [js-settings (:js project)
-	input-paths (map #(add-path (src-path project) %) inputs)
-	output-path (add-path (deploy-path project) output)
-	options (merge {:pretty-print devel}
-		       (:options js-settings)
-		       (if devel
-			 (:devel-options js-settings)
-			 (:prod-options js-settings)))]
+        input-paths (map #(add-path (src-path project) %) inputs)
+        output-path (add-path (deploy-path project) output)
+        options (merge {:pretty-print devel}
+                       (:options js-settings)
+                       (if devel
+                         (:devel-options js-settings)
+                         (:prod-options js-settings)))]
     (make-parents (file output-path))
     (println "Compiling" (apply str (interpose ", " inputs)) "...")
     (closure/run input-paths output-path options)))
 
+(defn- full-path [project path]
+  (str (get-in project [:js :src]) File/separator path))
+
+(defn- find-js-files [project path]
+  (if (.endsWith ".js" path)
+    (full-path project  path)
+    (let [f (File. (full-path project path))]
+      (if (.isDirectory f)
+        (find-js-files project (.getAbsolutePath f))))))
+
+(defn- parse-inputs [project inputs]
+  (for [input inputs]
+    (cond
+     (.endsWith input ".js")
+     input
+
+     (.isDirectory (File. (full-path project input)))
+     (remove empty? (flatten (find-js-files project input)))
+
+     :else
+     (throw (Exception. (str "invalid Js input:"))))))
+
 (defn js
-  ([project]
-     (js project "devel"))
+  ([project] (js project "devel"))
   ([project action]
-     (let [bundles (partition 2 (:bundles (:js project)))
-	   devel (not= action "prod")]
-       (doseq [[output inputs] bundles]
-	 (compile-bundle inputs output project devel)))))
+   (let [bundles (partition 2 (:bundles (:js project)))
+         devel (not= action "prod")]
+     (doseq [[output inputs] bundles]
+       #_(println "output" output "inputs:" (parse-inputs project inputs))
+       (compile-bundle inputs output project devel)))))
