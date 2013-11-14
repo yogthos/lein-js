725e80a1accdc8bad24d8b3fdfbff1d25807f4a0
Yogthos <dmitri.sotnikov@uhn.on.ca>
2013-11-14 15:48:20 -0500

    cleaned up dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   ;; Depends on the Google Closure Compiler, but it is not available on a Maven repo.
   ;; See http://code.google.com/p/closure-compiler/issues/detail?id=37
   ;; If it becomes available, I'll update this dependency to the official version.
-  :dependencies [;[org.clojure/google-closure-library "0.0-20130212-95c19e7f0f5f"]
+  :dependencies [
                  [com.google.javascript/closure-compiler "r1352"]]
   :dev-dependencies [[org.clojure/clojure "1.5.1"]])
